f0740d4672976376244f666000e70b3e8e59a7cc
AnassLat <anass.latrach@epitech.eu>
2020-12-12 21:30:49 +0100

    init2

--- README.md
+++
@@ -1,3 +0,0 @@
-# SimpleCrud
-
-Vu qu'on vas utiliser kotlin avec spring boot pour le backend de notre application mobile,j'ai consacré cette journé que pour l'apprentissage de ces deux technologie  
--- src/main/kotlin/com/tutorial/SimpleCrud/repositories/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.tutorial.SimpleCrud.repositories
-
-import com.tutorial.SimpleCrud.models.User
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository : JpaRepository<User, String>
